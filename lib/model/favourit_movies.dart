acad6f6e7ab3a22177224706163fc2b4f70caecf
ahmadaleqeili2 <gfayvifjychy@gmail.com>
2024-12-29 14:51:53 +0300

    fixing the error ayman dont work without tell me O_O

--- lib/model/favourit_movies.dart
+++ lib/model/favourit_movies.dart
@@ -1,6 +1,5 @@
 import 'package:just_movie/model/movie.dart';
 
-import 'user.dart';
 
 class FavouritMovies {
   List <Movie>? movies;
--- lib/view/initial_page.dart
+++ lib/view/initial_page.dart
@@ -2,46 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:just_movie/model/boxes.dart';
 import 'package:just_movie/model/login_token.dart';
 import 'package:just_movie/view/login_page.dart';
-import 'package:just_movie/view/signup_page.dart';
-
+import 'package:just_movie/view/move_between.dart';
 class InitialPage extends StatelessWidget {
   const InitialPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var dynamicToken = Boxes.boxToken.getAt(0);
+    var dynamicToken = Boxes.boxToken.isNotEmpty ? Boxes.boxToken.getAt(0) : null;
     LoginToken? token;
 
     if (dynamicToken is LoginToken) {
       token = dynamicToken;
-    } else {
+    } else if (dynamicToken != null) {
       print('Stored value is not a LoginToken.');
     }
+
     final currentDate = DateTime.now();
 
-    if (Boxes.boxToken.isEmpty)
+    if (Boxes.boxToken.isEmpty) {
       return LoginPage();
-    else if (currentDate.isAfter(token!.expiryDate)) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("return Login"),
-            content: Text("return login for your email"),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(); // Close the dialog
-                },
-                child: Text('OK'),
-              ),
-            ],
-          );
-        },
-      );
-      return LoginPage();
+    } else if (token != null && currentDate.isAfter(token.expiryDate)) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Session Expired"),
+              content: Text("Please log in again to continue."),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Close the dialog
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      });
+      return Container(); // Return an empty container while the dialog is shown
     } else {
-      return SignUpPage();
+      return HomePage();
     }
   }
 }
