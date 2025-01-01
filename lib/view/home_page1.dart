ccee5313c839fb3a0e4553d9585f7373953fa4a1
ahmadaleqeili2 <gfayvifjychy@gmail.com>
2025-01-01 20:46:13 +0300

    update : modifying the upper button in the home page

--- lib/view/home_page1.dart
+++ lib/view/home_page1.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:just_movie/colors.dart';
 import 'package:just_movie/constant/media_data.dart';
 import 'package:just_movie/controller/movie_controller.dart';
+import 'package:just_movie/model/media.dart';
 import 'package:just_movie/view/movie_list.dart';
 import 'package:just_movie/widgets/HomePageWidgets/main_image.dart';
 import 'package:just_movie/widgets/scrolled_list_widget.dart';
+
+List<Media> Alldata = MovieController.media;
+String WatsSelected = "All";
+int selectedButtonButtom = 0; // متابعة حالة الزر النشط
+int selectedButtonTop = 0;
 
 class HomePageBody extends StatefulWidget {
   @override
@@ -13,89 +19,108 @@ class HomePageBody extends StatefulWidget {
 }
 
 class _HomePageBodyState extends State<HomePageBody> {
-  int selectedButtonButtom = 0; // متابعة حالة الزر النشط
-  int selectedButtonTop = 0;
   int _currentIndex = 0;
   Timer? _timer;
 
   void _startAutoSlide() {
-  _timer = Timer.periodic(Duration(seconds: 3), (Timer timer) {
-    if (mounted) {
-      setState(() {
-        _currentIndex = (_currentIndex + 1) % mediaData.length;
-      });
-    } else {
-      timer.cancel();
-    }
-  });
-}
-
-@override
-initState() {
-  super.initState();
-  _startAutoSlide();
-}
-
-@override
-void dispose() {
-  _timer?.cancel();
-  super.dispose();
-}
-
-@override
-Widget build(BuildContext context) {
-  double screenHeight = MediaQuery.of(context).size.height;
-  double screenWidth = MediaQuery.of(context).size.width;
-
-  return Stack(
-    children: [
-      Container(
-        color: primarycolor,
-        height: screenHeight,
-        width: screenWidth,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              InkWell(
-                onTap: () {},
-                child: Container(
-                  height: screenHeight * 0.40,
-                  width: screenWidth,
-                  child: imageandbutton(
-                    currentIndex: _currentIndex,
-                    selectedButtonTop: selectedButtonTop,
-                    UperrButton: [
-                      () {},
-                      () {},
-                      () {},
-                      () {}
-                    ],
-                  ),
-                ),
-              ),
-              ScrolledListWidget(
-                items: MovieController.media,
-                title: "Recommended to you",
-                arrowPage: MovieListView(
-                  mediaData: MovieController.media,
-                ),
-                WithArrow: true,
-              ),
-              ScrolledListWidget(
-                items: MovieController.media.reversed.toList(),
-                title: "The Most Viewed",
-                arrowPage: MovieListView(
-                  mediaData: MovieController.media.reversed.toList(),
-                ),
-                WithArrow: true,
-              ),
-            ],
-          ),
-        ),
-      ),
-    ],
-  );
-}
-
+    _timer = Timer.periodic(Duration(seconds: 3), (Timer timer) {
+      if (mounted) {
+        setState(() {
+          _currentIndex = (_currentIndex + 1) % mediaData.length;
+        });
+      } else {
+        timer.cancel();
+      }
+    });
   }
 
+  @override
+  initState() {
+    super.initState();
+    _startAutoSlide();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Media> getDataWhere(String type, List<Media> AllData) {
+      if (type != "All") {
+        List<Media> media = [];
+        for (int i = 0; i < AllData.length; i++) {
+          if (AllData[i].type == type) {
+            media.add(AllData[i]);
+          }
+        }
+        return media;
+      } else {
+        return Alldata;
+      }
+    }
+
+    List<Media> fetchdata = getDataWhere(WatsSelected, Alldata);
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Stack(
+      children: [
+        Container(
+          color: primarycolor,
+          height: screenHeight,
+          width: screenWidth,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    height: screenHeight * 0.40,
+                    width: screenWidth,
+                    child: imageandbutton(
+                      currentIndex: _currentIndex,
+                      selectedButtonTop: selectedButtonTop,
+                      UperrButton: List.generate(
+                        4, // عدد الأنواع 4 فقط
+                        (index) => () {
+                          setState(() {
+                            selectedButtonTop = index;
+                            WatsSelected = [
+                              "All",
+                              "Film",
+                              "Tv show",
+                              "Series",
+                            ][index];
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                ScrolledListWidget(
+                  items: fetchdata,
+                  title: "Recommended to you",
+                  arrowPage: MovieListView(
+                    mediaData: fetchdata,
+                  ),
+                  WithArrow: true,
+                ),
+                ScrolledListWidget(
+                  items: fetchdata.reversed.toList(),
+                  title: "The Most Viewed",
+                  arrowPage: MovieListView(
+                    mediaData: fetchdata.reversed.toList(),
+                  ),
+                  WithArrow: true,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/HomePageWidgets/main_image.dart
+++ lib/widgets/HomePageWidgets/main_image.dart
@@ -3,12 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:just_movie/controller/movie_controller.dart';
 import 'package:just_movie/widgets/Buttons/scrolled_button_list.dart';
 
-// ignore: must_be_immutable
 class imageandbutton extends StatefulWidget {
-  int selectedButtonButtom = 0;
   final int currentIndex;
-  int selectedButtonTop;
-  List<void> UperrButton;
+  final int selectedButtonTop;
+  final List<void Function()> UperrButton;
+
   imageandbutton({
     Key? key,
     required this.currentIndex,
@@ -41,76 +40,18 @@ class _imageandbuttonState extends State<imageandbutton> {
             width: screenWidth,
           ),
         ),
-        Align(
-          alignment: Alignment(0, 1),
-          child: Container(
-            height: 60,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(50, 0, 0, 0),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(90),
-                bottomRight: Radius.circular(90),
-              ),
-            ),
-            child: Row(
-              spacing: 10,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(3, (index) {
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      widget.selectedButtonButtom = index;
-                    });
-                  },
-                  child: AnimatedContainer(
-                    duration: Duration(milliseconds: 200),
-                    width: 60,
-                    height: 4,
-                    decoration: BoxDecoration(
-                      color: widget.selectedButtonButtom == index
-                          ? const Color.fromARGB(
-                              140, 255, 255, 255) // أقل شفافية عند التحديد
-                          : const Color.fromARGB(
-                              50, 255, 255, 255), // أبيض شفاف عند عدم التحديد
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                  ),
-                );
-              }),
-            ),
-          ),
-        ),
         // الشريط العلوي للتمرير
         ScrolledButtonList(
           selectedButtonTop: widget.selectedButtonTop,
           withlogo: true,
-          BPress: [
-            () {
+          BPress: List.generate(
+            widget.UperrButton.length,
+            (index) => () {
               setState(() {
-                widget.selectedButtonTop = 0;
-                widget.UperrButton;
+                widget.UperrButton[index]!();
               });
             },
-            () {
-              setState(() {
-                widget.selectedButtonTop = 1;
-                widget.UperrButton;
-              });
-            },
-            () {
-              setState(() {
-                widget.selectedButtonTop = 2;
-                widget.UperrButton;
-              });
-            },
-            () {
-              setState(() {
-                widget.selectedButtonTop = 3;
-                widget.UperrButton;
-              });
-            },
-          ],
+          ),
         ),
       ],
     );
